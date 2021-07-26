c3256df8b5a7841ae9ceef0d62d77caa4749bfc7
Dieg Luna <diegofcolunalopez@gmail.com>
2021-07-26 10:58:14 -0500

    Modal: evento click con JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const modal = document.querySelector(".modal");
+const close = document.querySelector(".modal__content--close");
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+
+button1.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+});
+button2.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+});
+button3.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+});
+
+close.addEventListener("click", () => {
+  modal.classList.remove("visible");
+  modal.classList.add("hidden");
+});
